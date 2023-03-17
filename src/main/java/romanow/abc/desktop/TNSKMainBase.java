59bdfc9dc585c6dcc7c7d649688eadffff753169
romanow-nsk <romanow@ngs.ru>
2023-03-18 01:47:37 +0700

    Сервер+десктоп

--- src/main/java/romanow/abc/desktop/TNSKMainBase.java
+++ src/main/java/romanow/abc/desktop/TNSKMainBase.java
@@ -1,7 +1,10 @@
 package romanow.abc.desktop;
 
+import romanow.abc.core.constants.Values;
+
 public class TNSKMainBase {
     public static void main(String ss[]){
-
+        Values.init();
+        MainBase.main(null);
     }
 }
